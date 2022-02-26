3c7756c1d6aa96bc3b30b0baac73a57bb6cb7f31
Châu Hữu Phát <55749764+RobynChau@users.noreply.github.com>
2022-02-27 03:00:17 +0700

    Update iExpense
    Update iExpense to have a Sort Button (sort by default, by name, and by
    type) and a Search Bar

--- Playgrounds/Project 07 - iExpense.swiftpm/ContentView.swift
+++ Playgrounds/Project 07 - iExpense.swiftpm/ContentView.swift
@@ -1,12 +1,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum SortType {
+        case none, alphabetical, type
+    }
+    
     @StateObject var expenses = Expenses()
     @State private var showingAddExpense = false
+    @State private var showingSortOptions = false
+    @State private var sortingType = SortType.none
+    @State private var searchingText = ""
+    
     var body: some View {
         NavigationView{
             List{
-                ForEach(expenses.items) { item in
+                ForEach(sortedItems) { item in
                     HStack{
                         VStack(alignment: .leading){
                             Text(item.name)
@@ -14,21 +22,43 @@ struct ContentView: View {
                             Text(item.type)
                         }
                         Spacer()
+                        
                         Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                             .foregroundColor(color(forAmount: item.amount))
                     }
                     .accessibilityElement()
                     .accessibilityLabel("\(item.name) with amount \(item.amount)")
                     .accessibilityHint(item.type)
+                    
                 }
                 .onDelete(perform: removeItems)
+                
             }
+            .searchable(text: $searchingText)
             .navigationTitle("iExpense")
-            .toolbar {
-                Button{
-                    showingAddExpense = true
-                } label: {
-                    Image(systemName: "plus")
+            .navigationBarItems (
+                leading:
+                    Button {
+                        showingSortOptions = true
+                    } label: {
+                        Image(systemName: "arrow.up.arrow.down.circle")
+                    },
+                trailing:
+                    Button{
+                        showingAddExpense = true
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+            )
+            .confirmationDialog("Sort by", isPresented: $showingSortOptions) {
+                Button("Default") {
+                    sortingType = .none
+                }
+                Button("Alphabetical") {
+                    sortingType = .alphabetical
+                }
+                Button("Type") {
+                    sortingType = .type
                 }
             }
             .sheet(isPresented: $showingAddExpense) {
@@ -37,16 +67,53 @@ struct ContentView: View {
         }
     }
     func removeItems(at offsets: IndexSet){
-        expenses.items.remove(atOffsets: offsets)
-    }
-    func color(forAmount amount: Double) -> Color {
-        switch amount {
-        case 0..<10:
-            return .green
-        case 10..<100:
-            return .yellow
-        default:
-            return .red
+        let removeArray = offsets.enumerated()
+        for (_, c) in removeArray {
+            expenses.items.remove(at: expenses.items.firstIndex(where: {$0.id == sortedItems[c].id}) ?? 0)
         }
+    }
+    
+    var sortedItems: [ExpenseItem] {
+        var sortedItems: [ExpenseItem]
+        switch sortingType {
+        case .none:
+            sortedItems = expenses.items
+        case .alphabetical:
+            sortedItems = expenses.items.sorted {$0.name < $1.name}
+        case .type:
+            sortedItems = expenses.items.filter {$0.type == "Business"} + expenses.items.filter {$0.type == "Personal"}
+        }
+        if searchingText.isEmpty {
+            return sortedItems
+        } else {
+            return sortedItems.filter {$0.name.localizedCaseInsensitiveContains(searchingText)}
+        }
+        
+    }
+
+    
+    func color(forAmount amount: Double) -> Color {
+        if Locale.current.currencyCode == "USD" {
+            switch amount {
+            case 0..<10:
+                return .green
+            case 10..<100:
+                return .yellow
+            default:
+                return .red
+            }
+        } else if Locale.current.currencyCode == "VND" {
+            switch amount {
+            case 0..<100000:
+                return .green
+            case 100000..<2000000:
+                return .yellow
+            default:
+                return .red
+            }
+        } else {
+            return .white
+        }
+        
     }
 }
